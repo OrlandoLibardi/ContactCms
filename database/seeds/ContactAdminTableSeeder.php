6a74c11abd8dfb50164885a6d703ed22c3e8c0ed
Orlando Libardi <orlando.libardi@gmail.com>
2019-03-12 16:01:44 -0300

    Alterações estratégicas

--- database/seeds/ContactAdminTableSeeder.php
+++ database/seeds/ContactAdminTableSeeder.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Seeder;
 use OrlandoLibardi\PageCms\app\Page;
 use OrlandoLibardi\OlCms\AdminCms\app\Admin;
 
-class NewsletterAdminTableSeeder extends Seeder
+class ContactAdminTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
